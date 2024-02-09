b97e669b478c5d9bb4903ca1347352c26ca034e6
Ezequiel Melogno <melognator@gmail.com>
2024-02-09 17:33:37 -0300

    ?

--- src/app/(pages)/landing/page.jsx
+++ src/app/(pages)/landing/page.jsx
@@ -4,17 +4,16 @@ import LinkButton from '../../../components/common/buttons/LinkButton';
 export default function Landing() {
   return (
     <main>
-      <section className={styles.hero}>
         <h2>
-          Gestiona tus campañas de Dungeons & Dragons
+            Gestiona tus campañas de Dungeons & Dragons
         </h2>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
         </p>
         <LinkButton href={'/'}>
-          ¡Comienza tu aventura!
+            ¡Comienza tu aventura!
         </LinkButton>
-      </section>
     </main>
   );
 }
