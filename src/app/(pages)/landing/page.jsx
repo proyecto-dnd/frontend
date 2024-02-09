d56c9d5081b1a4d9919ba0ef6205393794d2cead
Ezequiel Melogno <melognator@gmail.com>
2024-02-09 17:41:20 -0300

    Arreglado CSS

--- src/app/(pages)/landing/page.jsx
+++ src/app/(pages)/landing/page.jsx
@@ -4,16 +4,20 @@ import LinkButton from '../../../components/common/buttons/LinkButton';
 export default function Landing() {
   return (
     <main>
-        <h2>
-            Gestiona tus campañas de Dungeons & Dragons
-        </h2>
-        <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
-        </p>
-        <LinkButton href={'/'}>
-            ¡Comienza tu aventura!
-        </LinkButton>
+        <section className={` section`}>
+            <div className={styles.hero + ' content'}>
+                <h2>
+                    Gestiona tus campañas de Dungeons & Dragons
+                </h2>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+                </p>
+                <LinkButton href={'/'}>
+                    ¡Comienza tu aventura!
+                </LinkButton>
+            </div>
+        </section>
     </main>
   );
 }
