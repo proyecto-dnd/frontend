e86a12abcf227d81c538b17fd0620f4bb7782776
Ezequiel Melogno <melognator@gmail.com>
2024-02-09 22:39:13 -0300

    Pequeños cambios

---
+++ src/components/sections/landing/Member/items.tsx
@@ -0,0 +1,17 @@
+import AllForOne from "@/components/icons/AllForOne";
+import DwarfFace from "@/components/icons/DwarfFace";
+
+const items = [
+  {
+    icon: <DwarfFace size={'6rem'}/>,
+    title: "Personajes",
+    text: "Juega con los personajes únicos que tenemos para tí.",
+  },
+  {
+    icon: <AllForOne size={'6rem'}/>,
+    title: "Campañas",
+    text: "Disfruta de campañas épicas diseñadas por nuestros expertos.",
+  },
+];
+
+export default items;
