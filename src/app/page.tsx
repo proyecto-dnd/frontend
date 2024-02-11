c5ea054ae480ebf085f4a5318efae5e8ef221cce
Ezequiel Melogno <melognator@gmail.com>
2024-02-11 16:49:11 -0300

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 'use client';
 import LinkButton from "@/components/common/buttons/Button";
 import { redirect, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
 
   const router = useRouter()
 
-  const user = window.localStorage.getItem('user')
-  if (!user) {
-    redirect('/landing');
-  }
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const user = window.localStorage.getItem('user')
+    if (!user) {
+      redirect('/landing');
+    }
+    setUser(user);
+  }, []);
+
 
   const handleLogout = () => {
     window.localStorage.removeItem('user');
