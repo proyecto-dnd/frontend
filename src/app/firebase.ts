8a2ae06d3173b40f05e575d71c703905c3d99274
javier-a-paredes <javi4202@gmail.com>
2024-02-21 01:52:36 -0300

    firebase initialize app - login handle function(login pop up) - cookie arrives in response but does not set itself to browser(CHECK)

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6ym1x8bQCnVHqPDXCVkBy8Ds7iiqmInk",
+  projectId: "logoflegends-19383",
+  appId: "1:487764884661:web:1cc8e754cc2f02de7592fc",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- src/components/sections/landing/Navbar/LoginPopup.tsx
+++ src/components/sections/landing/Navbar/LoginPopup.tsx
@@ -8,6 +8,9 @@ import InputPassword from '@/components/common/inputs/InputPassword'
 import Button from '@/components/common/buttons/Button'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { auth } from '@/context/authentication'
+import { signInWithEmailAndPassword  } from 'firebase/auth'
+import { json } from 'stream/consumers'
 
 const LoginPopup = () => {
 
@@ -20,11 +23,41 @@ const LoginPopup = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    
+    //LOGIN firebase
+    signInWithEmailAndPassword(auth, "prueba@gmail.com", "pass123").then(async userCredentials => {
+
+    // console.log(await userCredentials.user.getIdToken())
+
+    // localStorage.setItem("access_token", await userCredentials.user.getIdToken())    
+
+    let token = await userCredentials.user.getIdToken()
+
+    let data:  Record<string, string>= {
+      idToken: token
+    }
+
+    const jsonData = JSON.stringify(data)
+
+    // api login endpoint
+    fetch("http://localhost:8080/api/v1/user/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: jsonData,
+    }).then( res => console.log(res.body))// TODO .then redirect
+  }).catch((err) => {
+      let errorCode = err.code;
+      let errorMessage = err.message;
+      console.log(errorCode)
+      console.log(errorMessage)
+    })
     // TODO: Fetch
     // Hardcoded localstorage for testing
     const username = (e.target as HTMLFormElement).user.value
     window.localStorage.setItem('user', username)
-    router.push('/')
+    // router.push('/')
   }
 
   return (
@@ -40,9 +73,9 @@ const LoginPopup = () => {
       <form onSubmit={handleSubmit} className={`${styles.popup} ${show ? styles.show : styles.hide}`}>
           {/* <h3>Iniciar sesión</h3> */}
           <label htmlFor="user">Usuario</label>
-          <Input type="text" name="user" placeholder="Escribe aquí..." required />
+          <Input type="text" name="user" placeholder="Escribe aquí..."  />
           <label htmlFor="password">Contraseña</label>
-          <InputPassword name="password" placeholder="••••••••••" required />
+          <InputPassword name="password" placeholder="••••••••••"  />
           <Button type='submit' className={styles.submit}>Iniciar sesión</Button>
           <hr />
           <span>¿No tienes cuenta? <Link href={'/auth/signup'}>Regístrate</Link></span>
