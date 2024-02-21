b5d7dff8c886183e91fd37829b0833edf1ab1a71
Ezequiel Melogno <melognator@gmail.com>
2024-02-21 19:13:26 -0300

    Arreglando card campaign

--- src/components/home/CardCampaign/CardCampaign.tsx
+++ src/components/home/CardCampaign/CardCampaign.tsx
@@ -11,7 +11,7 @@ export type CardCampaignProps = {
 const CardCampaign = ({ img, title, description }: CardCampaignProps) => {
   return (
     <article className={styles.cardCampaign}>
-      <section><Image src={img} alt='title' width={300} height={212}/></section>
+      <section className={styles.image}><Image src={img} alt='title' fill={true}/></section>
       <section className={styles.information}>
         <h3 className={styles.h3}>{title}</h3>
         <p className={styles.p}>{description}</p>
