8146e1b66d9c82b7e5a6941656a6ee104f28e1b3
Ezequiel Melogno <melognator@gmail.com>
2024-03-05 19:12:07 -0300

    Cambios al authform

--- src/components/auth/SignUpForm/SignUpForm.tsx
+++ src/components/auth/SignUpForm/SignUpForm.tsx
@@ -3,8 +3,10 @@ import Button from "@/components/common/buttons/Button";
 import Input from "@/components/common/inputs/Input";
 import InputPassword from "@/components/common/inputs/InputPassword";
 import styles from "./SignUpForm.module.css";
+import formStyles from "../AuthForm/AuthForm.module.css";
 import Link from "next/link";
 import useLogin from "@/hooks/useLogin";
+import FormGroup from "@/components/home/NewLayout/FormGroup";
 
 const SignUpForm = () => {
 
@@ -21,82 +23,78 @@ const SignUpForm = () => {
 
   return (
     <>
-      <h1 className={styles.h1}>Crear cuenta</h1>
-      <p className={styles.text}>
-        ¡Bienvenido! Ingresa tus datos para formar parte de{" "}
-        <span className={styles.span}>DiceLogger</span>
+      <h1>Crear cuenta</h1>
+      <p className={formStyles.description}>
+        ¡Bienvenido! <br/>
+        Ingresa tus datos para formar parte de <strong>DiceLogger</strong>
       </p>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.container}>
-          <div className={styles.box}>
-            <label htmlFor="name" className={styles.label}>
-              Nombre
+      <form onSubmit={handleSubmit} className={formStyles.form}>
+        <section className={styles.container}>
+          <FormGroup>
+            <label htmlFor="user" className={styles.label}>
+              Nombre de usuario
             </label>
             <Input
               type="text"
-              name="name"
-              placeholder="Doe"
+              name="user"
+              placeholder="johndoe17"
               required
               className={styles.input}
             />
-          </div>
-          <div className={styles.box}>
-            <label htmlFor="lastName" className={styles.label}>
-              Apellido
+          </FormGroup>
+          <FormGroup>
+            <label htmlFor="displayName" className={styles.label}>
+              Nombre de display
             </label>
             <Input
               type="text"
-              name="lastName"
+              name="displayName"
               placeholder="Jhon"
               required
               className={styles.input}
             />
-          </div>
-        </div>
-        <label htmlFor="user" className={styles.label}>
-          Username
-        </label>
-        <Input
-          type="text"
-          name="user"
-          placeholder="johndoe17"
-          required
-          className={styles.input}
-        />
-        <label htmlFor="email" className={styles.label}>
-          Correo electrónico
-        </label>
-        <Input
-          type="email"
-          name="email"
-          placeholder="johndoe@gmail.com"
-          required
-          className={styles.input}
-        />
-        <label htmlFor="password" className={styles.label}>
-          Contraseña
-        </label>
-        <InputPassword
-          name="password"
-          placeholder="••••••••••"
-          required
-          className={styles.input}
-        />
-        <label htmlFor="repitPassword" className={styles.label}>
-          Repetir contraseña
-        </label>
-        <InputPassword
-          name="repitPassword"
-          placeholder="••••••••••"
-          required
-          className={styles.input}
-        />
-        <div className={styles.log}>
+          </FormGroup>
+        </section>
+        <FormGroup>
+          <label htmlFor="email" className={styles.label}>
+            Correo electrónico
+          </label>
+          <Input
+            type="email"
+            name="email"
+            placeholder="johndoe@gmail.com"
+            required
+            className={styles.input}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="password" className={styles.label}>
+            Contraseña
+          </label>
+          <InputPassword
+            name="password"
+            placeholder="••••••••••"
+            required
+            className={styles.input}
+          />
+        </FormGroup>
+        {/* <FormGroup>
+          <label htmlFor="repitPassword" className={styles.label}>
+            Repetir contraseña
+          </label>
+          <InputPassword
+            name="repitPassword"
+            placeholder="••••••••••"
+            required
+            className={styles.input}
+          />
+        </FormGroup> */}
+        <div className={formStyles.buttons}>
           <Button type="submit" className={styles.submit}>
             Crear cuenta
           </Button>
-          <span style={{ marginTop: "20px" }}>
-          ¿Ya tienes cuenta? <Link href={"/auth/login"}>Inicia Sesión</Link>
+          <span className={formStyles.navigation}>
+            ¿Ya tienes cuenta? <Link href={"/auth/login"}>Inicia Sesión</Link>
           </span>
         </div>
       </form>
