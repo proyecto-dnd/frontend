1b80ac3abec92ef93c34de04ad0a4ac655e9f9e7
BautistaOnorato <bautistaonorato27@gmail.com>
2024-03-29 19:16:30 -0300

    resolviendo conflictos

--- src/app/(home)/campaigns/page.tsx
+++ src/app/(home)/campaigns/page.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import CampaignList from "@/components/home/Campaign/CampaignList/CampaignList";
 
-// export const revalidate = 0;
+export const revalidate = 0;
 
-// const getCampaigns = async () => {
-//   const data = {
-//     campaigns: [],
-//     info: "",
-//   };
-//   try {
-//     const response = await fetch(process.env.URL + "/api/campaigns");
-//     data.campaigns = await response.json();
-//     data.info = "Success";
-//   } catch (error: any) {
-//     data.info = error.message;
-//   }
-//   return data;
-// };
+const getCampaigns = async () => {
+  const data = {
+    campaigns: [],
+    info: "",
+  };
+  try {
+    const response = await fetch(process.env.URL + "/api/campaigns");
+    data.campaigns = await response.json();
+    data.info = "Success";
+  } catch (error: any) {
+    data.info = error.message;
+  }
+  return data;
+};
 
 const Campaigns = async () => {
   return <CampaignList />;
