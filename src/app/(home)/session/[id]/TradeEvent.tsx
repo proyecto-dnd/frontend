4319873ce7070b2f5d6cdba3f0a01b94cc2b2b25
alvarezgonzalo0022 <alvarezgonzalo0022@gmail.com>
2024-03-30 23:56:48 -0300

    TradeEvent refactor

--- src/app/(home)/session/[id]/TradeEvent.tsx
+++ src/app/(home)/session/[id]/TradeEvent.tsx
@@ -6,19 +6,26 @@ import { useState } from "react";
 import EventForm from "./EventForm";
 import styles from "./page.module.css";
 import MultiSelect from "@/components/common/inputs/MultiSelect";
+import { log } from "console";
 
 const TradeEvent = () => {
-  const [characters, setCharacters] = useState([]);
-  const [selectedCharacter, setSelectedCharacter] = useState<
-    string | undefined
-  >(undefined);
-  const [selectedCharacterItemsToTrade, setSelectedCharacterItemsToTrade] =
-    useState([]);
-  const [myItemsToTrade, setMyItemsToTrade] = useState([]);
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [selectedCharacter, setSelectedCharacter] = useState<string | undefined>(undefined);
+  const [selectedCharacterItemsToTrade, setSelectedCharacterItemsToTrade] = useState<Item[]>([]);
+  const [myItemsToTrade, setMyItemsToTrade] = useState<Item[]>([]);
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
-  const [selectedCharacterItems, setSelectedCharacterItems] = useState<
-    string[]
-  >([]);
+  const [selectedCharacterItems, setSelectedCharacterItems] = useState<string[]>([]);
+
+  interface Item {
+    value: string;
+    label: string;
+  }
+
+  interface Character {
+    character_id: string;
+    user_id: string;
+    name: string;
+  }  
 
   const mergeUniqueItems = (oldItems: any, newItems: any) => {
     const combined = [...oldItems, ...newItems];
@@ -29,86 +36,123 @@ const TradeEvent = () => {
     );
   };
 
-  const getCharacters = async () => {
-    const response = await fetch(
-      `http://localhost:8080/api/v1/character/filter?campaignid=1`
-    );
+  const getRequest = async (url: string) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     const data = await response.json();
+    return data;
+  }
+
+  const getCharacters = async () => {
+    const data = await getRequest(`http://localhost:8080/api/v1/character/filter?campaignid=1`);
     setCharacters(data);
   };
 
-  const getItems = async (userData: any) => {
-    const user = await userData;
+  const getMyCharacter = async (userData: any) => {
+    const user = await userData;        
 
     const myCharacter = characters.filter(
       (character: any) => character.user_id === user.id
     );
 
-    if (myCharacter.length === 0) return;
+    if (myCharacter.length === 0) return;    
 
-    const itemsCharacter = await fetch(
-      `http://localhost:8080/api/v1/item_character/character/${myCharacter[0].character_id}`
-    );
-    const data = await itemsCharacter.json();
+    return myCharacter[0].character_id;
+  }
+
+  const getItems = async (userData: any) => {
+
+    const myCharacter = await getMyCharacter(userData);  
+    
+    if (!myCharacter) return;
+
+    const data = await getRequest(`http://localhost:8080/api/v1/item_character/character/${myCharacter}`);
 
     const itemsToTrade = data.map((item: any) => ({
-      value: item.item.item_id,
+      value: `${item.item.item_id}-item`,
       label: item.item.name,
+      type: 'item'
     }));
 
     setMyItemsToTrade((prevState) => mergeUniqueItems(prevState, itemsToTrade));
   };
 
   const getWeapons = async (userData: any) => {
-    const user = await userData;
 
-    const myCharacter = characters.filter(
-      (character: any) => character.user_id === user.id
-    );
+    const myCharacter = await getMyCharacter(userData);
 
-    if (myCharacter.length === 0) return;
+    if (!myCharacter) return;
 
-    const response = await fetch(
-      `http://localhost:8080/api/v1/weapon_character/character/${myCharacter[0].character_id}`
-    );
-    const data = await response.json();
+    const data = await getRequest(`http://localhost:8080/api/v1/weapon_character/character/${myCharacter}`);
 
     const weaponsToTrade = data.map((weapon: any) => ({
-      value: weapon.weapon.weapon_id,
+      value: `${weapon.weapon.weapon_id}-weapon`,
       label: weapon.weapon.name,
+      type: 'weapon'
     }));
 
-    setMyItemsToTrade((prevState) =>
-      mergeUniqueItems(prevState, weaponsToTrade)
-    );
+    setMyItemsToTrade((prevState) => mergeUniqueItems(prevState, weaponsToTrade));
   };
 
   const getArmors = async (userData: any) => {
-    const user = await userData;
 
-    const myCharacter = characters.filter(
-      (character: any) => character.user_id === user.id
-    );
+    const myCharacter = await getMyCharacter(userData);
 
-    if (myCharacter.length === 0) return;
-    const response = await fetch(
-      `http://localhost:8080/api/v1/armor_character/character/${myCharacter[0].character_id}`
-    );
-    const data = await response.json();
+    if (!myCharacter) return;
+
+    const data = await getRequest(`http://localhost:8080/api/v1/armor_character/character/${myCharacter}`);
 
     const armorsToTrade = data.map((armor: any) => ({
-      value: armor.armor.armor_id,
+      value: `${armor.armor.armor_id}-armor`,
       label: armor.armor.name,
+      type: 'armor'
     }));
 
-    setMyItemsToTrade((prevState) =>
-      mergeUniqueItems(prevState, armorsToTrade)
-    );
+    setMyItemsToTrade((prevState) => mergeUniqueItems(prevState, armorsToTrade));
   };
 
+  const getSelectedCharacterItemsData = async () => {
+    const selectedCharacterItemsData = await getRequest(`http://localhost:8080/api/v1/item_character/character/${selectedCharacter}`)
+    const selectedCharacterItemsToTrade = selectedCharacterItemsData.map(
+      (item: any) => ({
+        value: `${item.item.item_id}-item`,
+        label: item.item.name,
+        type: 'item'
+      })
+    );
+    setSelectedCharacterItemsToTrade((prevState) => mergeUniqueItems(prevState, selectedCharacterItemsToTrade));
+  }
+
+  const getSelectedCharacterWeaponsData = async () => {
+    const selectedCharacterWeaponsData = await getRequest(`http://localhost:8080/api/v1/weapon_character/character/${selectedCharacter}`);
+    const weaponsSelectedCharacter = selectedCharacterWeaponsData.map(
+      (weapon: any) => ({
+        value: `${weapon.weapon.weapon_id}-weapon`,
+        label: weapon.weapon.name,
+        type: 'weapon'
+      })
+    );
+    setSelectedCharacterItemsToTrade((prevState) => mergeUniqueItems(prevState, weaponsSelectedCharacter));
+  }
+
+  const getSelectedCharacterArmorsData = async () => {
+    const selectedCharacterArmorsData = await getRequest(`http://localhost:8080/api/v1/armor_character/character/${selectedCharacter}`);
+    const armorsSelectedCharacter = selectedCharacterArmorsData.map(
+      (armor: any) => ({
+        value: `${armor.armor.armor_id}-armor`,
+        label: armor.armor.name,
+        type: 'armor'
+      })
+    );
+    setSelectedCharacterItemsToTrade((prevState) => mergeUniqueItems(prevState, armorsSelectedCharacter));
+  }
+
   const handleItems = (value: string) => {
+    
     if (selectedItems.includes(value)) {
-      setSelectedItems(selectedItems.filter((skill) => skill !== value));
+      setSelectedItems(selectedItems.filter((item) => item !== value));
     } else {
       setSelectedItems([...selectedItems, value]);
     }
@@ -117,10 +161,10 @@ const TradeEvent = () => {
   const handleCharacterSelectedItems = (value: string) => {
     if (selectedCharacterItems.includes(value)) {
       setSelectedCharacterItems(
-        selectedCharacterItems.filter((skill) => skill !== value)
+        selectedCharacterItems.filter((item) => item !== value)
       );
     } else {
-      setSelectedCharacterItems([...selectedItems, value]);
+      setSelectedCharacterItems([...selectedCharacterItems, value]);
     }
   };
 
@@ -128,10 +172,13 @@ const TradeEvent = () => {
     getCharacters();
   }, []);
 
-  useEffect(() => {
+  useEffect(() => { 
+
+    if (!characters) return;
+
     const getUserData = async () => {
-      const response = await fetch("/api/my", {});
-      const data = await response.json();
+      const res = await fetch(`/api/my`);
+      const data = await res.json();
       return data;
     };
 
@@ -143,51 +190,15 @@ const TradeEvent = () => {
 
   useEffect(() => {
     const getSelectedCharacterData = async () => {
-      const response = await fetch(
-        `http://localhost:8080/api/v1/item_character/character/${selectedCharacter}`
-      );
-      const selectedCharacterItemsData = await response.json();
-      const selectedCharacterItemsToTrade = selectedCharacterItemsData.map(
-        (item: any) => ({
-          value: item.item.item_id,
-          label: item.item.name,
-        })
-      );
-      setSelectedCharacterItemsToTrade((prevState) =>
-        mergeUniqueItems(prevState, selectedCharacterItemsToTrade)
-      );
-
-      const responseWeaponsSelectedCharacter = await fetch(
-        `http://localhost:8080/api/v1/weapon_character/character/${selectedCharacter}`
-      );
-      const selectedCharacterWeaponsData =
-        await responseWeaponsSelectedCharacter.json();
-      const weaponsSelectedCharacter = selectedCharacterWeaponsData.map(
-        (weapon: any) => ({
-          value: weapon.weapon.weapon_id,
-          label: weapon.weapon.name,
-        })
-      );
-      setSelectedCharacterItemsToTrade((prevState) =>
-        mergeUniqueItems(prevState, weaponsSelectedCharacter)
-      );
-
-      const responseArmorSelectedCharacter = await fetch(
-        `http://localhost:8080/api/v1/armor_character/character/1`
-      );
-      const selectedCharacterData = await responseArmorSelectedCharacter.json();
-      const armorsSelectedCharacter = selectedCharacterData.map(
-        (armor: any) => ({
-          value: armor.armor.armor_id,
-          label: armor.armor.name,
-        })
-      );
-      setSelectedCharacterItemsToTrade((prevState) =>
-        mergeUniqueItems(prevState, armorsSelectedCharacter)
-      );
+      await getSelectedCharacterItemsData();
+      await getSelectedCharacterWeaponsData();
+      await getSelectedCharacterArmorsData();
     };
 
-    if (selectedCharacter) getSelectedCharacterData();
+    if (selectedCharacter) {
+      setSelectedCharacterItems([]);
+      getSelectedCharacterData();
+    }
   }, [selectedCharacter]);
 
   return (
